38b9a1f25e5e5c6514a8c7ceeda704e6facfedac
JONUSharma <jonusharam4440@gmail.com>
2024-09-16 23:19:17 +0530

    again some changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Jonu_portfolio/",
   plugins: [react()],
-  base: "/Jonu_portfolio",
 })
